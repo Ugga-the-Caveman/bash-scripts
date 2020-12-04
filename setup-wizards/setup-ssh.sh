e3afed25331e4f8b46b1c4b529e1b4fd5a0332a3
Ugga <42067940+Ugga-the-Caveman@users.noreply.github.com>
2020-12-04 01:19:07 +0100

    Update setup-ssh.sh

--- setup-wizards/setup-ssh.sh
+++ setup-wizards/setup-ssh.sh
@@ -68,8 +68,22 @@ then
 	exit
 fi
 
-usermod -a -G $groupName $benutzername
+gpasswd -a $benutzername $groupName
 
+
+echo ""
+mkdir -v /home/$benutzername/.ssh
+
+echo "Generate a keypair on your machine. Enter the public key to append it to authorized_keys"
+
+read -p "Enter username: " publicKey
+
+if [ "$publicKey" == "" ]
+then
+	echo "No key entered. Check /home/$benutzername/.ssh/authorized_keys"
+else
+	echo $publicKey > /home/$benutzername/.ssh/authorized_keys
+fi
 
 
 
