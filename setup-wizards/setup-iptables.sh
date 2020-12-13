fa5f8c4afd9844f52b800415f9b917c438a0799a
Ugga <42067940+Ugga-the-Caveman@users.noreply.github.com>
2020-12-13 02:50:35 +0100

    Update setup-iptables.sh

--- setup-wizards/setup-iptables.sh
+++ setup-wizards/setup-iptables.sh
@@ -13,9 +13,6 @@ echo "+-------------------------------+"
 echo "| iptables Configuration Script |"
 echo "+-------------------------------+"
 echo ""
-echo "This script will setup a simple stateful firewall configuration."
-echo ""
-
 
 
 sshPort=$(cat /etc/ssh/sshd_config | grep Port | sed 's/[^0-9]*//g')
@@ -73,28 +70,24 @@ iptables -A INPUT -i lo -j ACCEPT
 #iptables -A INPUT -p 41 -j ACCEPT
 
 
-
 #DROP traffic with an "INVALID" state match.
 iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+
+
+iptables -N CUSTOM
+iptables -A INPUT -j CUSTOM
 
 
 #alle eingehenden packete aktzeptieren, die zu einer verbindung gehören, die schon aufgebaut ist.
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 
-#The next rule will accept all new incoming ICMP echo requests, also known as pings.
-iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
-
-
-#Now we attach the TCP and UDP chains to the INPUT chain to handle all new incoming connections.
+#Now we attach the NEW chain to the INPUT chain to handle all new incoming connections.
 #Once a connection is accepted by either TCP or UDP chain, it is handled by the RELATED/ESTABLISHED traffic rule.
 
-iptables -N TCP
-iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
-
-iptables -N UDP
-iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
-
+iptables -N NEW
+iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j NEW
+iptables -A INPUT -p udp -m conntrack --ctstate NEW -j NEW
 
 
 #We reject TCP connections with TCP RESET packets and UDP streams with ICMP port unreachable messages if the ports are not opened.
@@ -148,24 +141,22 @@ ip6tables -A INPUT -p 41 -j ACCEPT
 ip6tables -A INPUT -m conntrack --ctstate INVALID -j DROP
 
 
+ip6tables -N CUSTOM
+ip6tables -A INPUT -j CUSTOM
+
+
 #alle eingehenden packete aktzeptieren, die zu einer verbindung gehören, die schon aufgebaut ist.
 
 ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 
-#The next rule will accept all new incoming ICMP echo requests, also known as pings.
 
-ip6tables -A INPUT -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT
-
-
-#Now we attach the TCP and UDP chains to the INPUT chain to handle all new incoming connections.
+#Now we attach the NEW chains to the INPUT chain to handle all new incoming connections.
 #Once a connection is accepted by either TCP or UDP chain, it is handled by the RELATED/ESTABLISHED traffic rule.
 
-ip6tables -N TCP
-ip6tables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
-
-ip6tables -N UDP
-ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
+ip6tables -N NEW
+ip6tables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j NEW
+ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j NEW
 
 
 #We reject TCP connections with TCP RESET packets and UDP streams with ICMP port unreachable messages if the ports are not opened.
@@ -180,23 +171,26 @@ ip6tables -A INPUT -p udp -j REJECT --reject-with icmp6-adm-prohibited
 
 ip6tables -A INPUT -j REJECT --reject-with icmp6-adm-prohibited
 
-#---
 
 
 
-echo "Setting up SSH configuration..."
+#The next rule will accept all new incoming ICMP echo requests, also known as pings.
+iptables -A CUSTOM -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+ip6tables -A CUSTOM -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT
 
 
+
+
+echo "Creating SSH chain..."
 
 iptables -N SSH
-iptables -A TCP -p tcp --dport $sshPort -j SSH
+iptables -A NEW -p tcp --dport $sshPort -j SSH
 iptables -A SSH -j ACCEPT
 
 
 ip6tables -N SSH
-ip6tables -A TCP -p tcp --dport $sshPort -j SSH
+ip6tables -A NEW -p tcp --dport $sshPort -j SSH
 ip6tables -A SSH -j ACCEPT
-
 
 
 
@@ -215,9 +209,4 @@ ip6tables -P OUTPUT ACCEPT
 
 
 
-echo "Setup complete."
-
-
-
-echo ""
-echo "Keep in Mind, that you must save the configuration to make it persistent."
+echo "iptables setup completed."
