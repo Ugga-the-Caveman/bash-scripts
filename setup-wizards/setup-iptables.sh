dbf4b515f228459e0a4af580846c1b78fad94f1b
Ugga-the-Caveman <42067940+Ugga-the-Caveman@users.noreply.github.com>
2021-02-03 02:36:47 +0100

    setup-iptables kann jetzt beide, oder nur ipv4/ipv6 regeln einstellen. option_allowpings added

--- setup-wizards/setup-iptables.sh
+++ setup-wizards/setup-iptables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-version="2021.01.31"
+version="2021.02.03"
 scriptName=$(basename $BASH_SOURCE)
 
 function fnc_printTitle()
@@ -16,15 +16,13 @@ function fnc_printTitle()
 
 function fnc_printHelp()
 {
-	echo "Description: setup iptables for ipv4 and ipv6."
-	echo "The default action is removing all iptables rules and then setup a simple statefull Firewall"
+	echo "Description: setup a simple statefull firewall with iptables."
 	echo ""
 	echo "Usage: $scriptName [Option]..."
 	echo ""
 	echo "Options:"
 	echo " -v,--version		prints script version"
 	echo " -h,--help		prints this help message"
-	echo " -f,--flush		Only remove all rules from tables."
 	echo ""
 	exit
 }
@@ -93,6 +91,39 @@ fi
 
 
 
+echo "Select IP Protocol"
+echo "which iptables do you want to change?"
+
+sources=("both" "only ipv4" "only ipv6")
+
+select selectedProtocols in "${sources[@]}"
+do
+	if [ "$REPLY" -ge "1" ] && [ "$REPLY" -le ${#sources[@]} ];
+	then
+		break;
+	else
+		echo "Bad Input. Enter a number between 1 and ${#sources[@]}."
+	fi
+done
+
+echo ""
+
+
+
+#allow pings?
+option_allowPings=true
+
+read -p "Do you want to allow incoming pings? [Y/n] " answer
+
+if [ "${answer:0:1}" == "n" ] || [ "${answer:0:1}" == "N" ]
+then
+        option_allowPings=false
+fi
+
+echo ""
+
+
+
 
 #get port for ssh
 sshPort=""
@@ -136,158 +167,178 @@ fi
 
 
 
-
 echo ""
-echo "Setting up IPv4 configuration..."
+
+if [ "$selectedProtocols" != "1" ]
+then
+	echo "Setting up IPv4 configuration..."
 
 
+	iptables -F
+	iptables -X
+	iptables -t nat -F
+	iptables -t nat -X
+	iptables -t mangle -F
+	iptables -t mangle -X
+	iptables -t raw -F
+	iptables -t raw -X
+	iptables -t security -F
+	iptables -t security -X
 
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -t raw -F
-iptables -t raw -X
-iptables -t security -F
-iptables -t security -X
-
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-
-
-#loopback verbindungen auf dem loopbackdevice erlauben
-iptables -A INPUT -i lo -j ACCEPT
+	iptables -P INPUT ACCEPT
+	iptables -P FORWARD ACCEPT
+	iptables -P OUTPUT ACCEPT
 
 
-#ICMPv6 Neighbor Discovery packets will always be classified "INVALID" though they are not corrupted or the like.
-#Accept them before dropping traffic with an "INVALID" state match.
-#iptables -A INPUT -p 41 -j ACCEPT
+	#loopback verbindungen auf dem loopbackdevice erlauben
+	iptables -A INPUT -i lo -j ACCEPT
 
 
-#DROP traffic with an "INVALID" state match.
-iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+	#ICMPv6 Neighbor Discovery packets will always be classified "INVALID" though they are not corrupted or the like.
+	#Accept them before dropping traffic with an "INVALID" state match.
+	#iptables -A INPUT -p 41 -j ACCEPT
 
 
-iptables -N CUSTOM
-iptables -A INPUT -j CUSTOM
+	#DROP traffic with an "INVALID" state match.
+	iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 
 
-#alle eingehenden packete aktzeptieren, die zu einer verbindung gehören, die schon aufgebaut ist.
-iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+	iptables -N CUSTOM
+	iptables -A INPUT -j CUSTOM
 
 
-#Now we attach the NEW chain to the INPUT chain to handle all new incoming connections.
-#Once a connection is accepted by either TCP or UDP chain, it is handled by the RELATED/ESTABLISHED traffic rule.
-
-iptables -N NEW
-iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j NEW
-iptables -A INPUT -p udp -m conntrack --ctstate NEW -j NEW
+	#alle eingehenden packete aktzeptieren, die zu einer verbindung gehören, die schon aufgebaut ist.
+	iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 
-#We reject TCP connections with TCP RESET packets and UDP streams with ICMP port unreachable messages if the ports are not opened.
-#This imitates default Linux behavior (RFC compliant), and it allows the sender to quickly close the connection and clean up.
+	#Now we attach the NEW chain to the INPUT chain to handle all new incoming connections.
+	#Once a connection is accepted by either TCP or UDP chain, it is handled by the RELATED/ESTABLISHED traffic rule.
 
-iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
-iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+	iptables -N NEW
+	iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j NEW
+	iptables -A INPUT -p udp -m conntrack --ctstate NEW -j NEW
 
 
-#For other protocols, we add a final rule to the chain to reject all remaining incoming traffic with icmp protocol unreachable messages.
-#This imitates Linux's default behavior.
+	#We reject TCP connections with TCP RESET packets and UDP streams with ICMP port unreachable messages if the ports are not opened.
+	#This imitates default Linux behavior (RFC compliant), and it allows the sender to quickly close the connection and clean up.
 
-iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+	iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+	iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
 
+
+	#For other protocols, we add a final rule to the chain to reject all remaining incoming traffic with icmp protocol unreachable messages.
+	#This imitates Linux's default behavior.
+
+	iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+
+fi
 #---
 
 
-echo "Setting up IPv6 configuration..."
+
+if [ "$selectedProtocols" != "2" ]
+then
+	echo "Setting up IPv6 configuration..."
 
 
-ip6tables -F
-ip6tables -X
-ip6tables -t nat -F
-ip6tables -t nat -X
-ip6tables -t mangle -F
-ip6tables -t mangle -X
-ip6tables -t raw -F
-ip6tables -t raw -X
-ip6tables -t security -F
-ip6tables -t security -X
+	ip6tables -F
+	ip6tables -X
+	ip6tables -t nat -F
+	ip6tables -t nat -X
+	ip6tables -t mangle -F
+	ip6tables -t mangle -X
+	ip6tables -t raw -F
+	ip6tables -t raw -X
+	ip6tables -t security -F
+	ip6tables -t security -X
 
 
-ip6tables -P INPUT ACCEPT
-ip6tables -P FORWARD ACCEPT
-ip6tables -P OUTPUT ACCEPT
+	ip6tables -P INPUT ACCEPT
+	ip6tables -P FORWARD ACCEPT
+	ip6tables -P OUTPUT ACCEPT
 
 
-#loopback verbindungen auf dem loopbackdevice erlauben
+	#loopback verbindungen auf dem loopbackdevice erlauben
 
-ip6tables -A INPUT -i lo -j ACCEPT
-
-
-#ICMPv6 Neighbor Discovery packets will always be classified "INVALID" though they are not corrupted or the like.
-#Accept them before dropping traffic with an "INVALID" state match.
-
-ip6tables -A INPUT -p 41 -j ACCEPT
+	ip6tables -A INPUT -i lo -j ACCEPT
 
 
-#DROP traffic with an "INVALID" state match.
+	#ICMPv6 Neighbor Discovery packets will always be classified "INVALID" though they are not corrupted or the like.
+	#Accept them before dropping traffic with an "INVALID" state match.
 
-ip6tables -A INPUT -m conntrack --ctstate INVALID -j DROP
-
-
-ip6tables -N CUSTOM
-ip6tables -A INPUT -j CUSTOM
+	ip6tables -A INPUT -p 41 -j ACCEPT
 
 
-#alle eingehenden packete aktzeptieren, die zu einer verbindung gehören, die schon aufgebaut ist.
+	#DROP traffic with an "INVALID" state match.
 
-ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-
-
-
-#Now we attach the NEW chains to the INPUT chain to handle all new incoming connections.
-#Once a connection is accepted by either TCP or UDP chain, it is handled by the RELATED/ESTABLISHED traffic rule.
-
-ip6tables -N NEW
-ip6tables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j NEW
-ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j NEW
+	ip6tables -A INPUT -m conntrack --ctstate INVALID -j DROP
 
 
-#We reject TCP connections with TCP RESET packets and UDP streams with ICMP port unreachable messages if the ports are not opened.
-#This imitates default Linux behavior (RFC compliant), and it allows the sender to quickly close the connection and clean up.
-
-ip6tables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
-ip6tables -A INPUT -p udp -j REJECT --reject-with icmp6-adm-prohibited
+	ip6tables -N CUSTOM
+	ip6tables -A INPUT -j CUSTOM
 
 
-#For other protocols, we add a final rule to the chain to reject all remaining incoming traffic with icmp protocol unreachable messages.
-#This imitates Linux's default behavior.
+	#alle eingehenden packete aktzeptieren, die zu einer verbindung gehören, die schon aufgebaut ist.
 
-ip6tables -A INPUT -j REJECT --reject-with icmp6-adm-prohibited
+	ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 
 
 
+	#Now we attach the NEW chains to the INPUT chain to handle all new incoming connections.
+	#Once a connection is accepted by either TCP or UDP chain, it is handled by the RELATED/ESTABLISHED traffic rule.
 
-#The next rule will accept all new incoming ICMP echo requests, also known as pings.
-iptables -A CUSTOM -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
-ip6tables -A CUSTOM -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT
-
-
-
-
-echo "Creating SSH chain..."
-
-iptables -N SSH
-iptables -A NEW -p tcp --dport $sshPort -j SSH
-iptables -A SSH -j ACCEPT
+	ip6tables -N NEW
+	ip6tables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j NEW
+	ip6tables -A INPUT -p udp -m conntrack --ctstate NEW -j NEW
 
 
-ip6tables -N SSH
-ip6tables -A NEW -p tcp --dport $sshPort -j SSH
-ip6tables -A SSH -j ACCEPT
+	#We reject TCP connections with TCP RESET packets and UDP streams with ICMP port unreachable messages if the ports are not opened.
+	#This imitates default Linux behavior (RFC compliant), and it allows the sender to quickly close the connection and clean up.
+
+	ip6tables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+	ip6tables -A INPUT -p udp -j REJECT --reject-with icmp6-adm-prohibited
+
+
+	#For other protocols, we add a final rule to the chain to reject all remaining incoming traffic with icmp protocol unreachable messages.
+	#This imitates Linux's default behavior.
+
+	ip6tables -A INPUT -j REJECT --reject-with icmp6-adm-prohibited
+
+fi
+
+
+
+echo "Adding custom rules..."
+
+
+if [ $option_allowPings == true ]
+then
+	if [ "$selectedProtocols" != "1" ]
+	then
+		iptables -A CUSTOM -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+	fi
+
+	if [ "$selectedProtocols" != "2" ]
+	then
+		ip6tables -A CUSTOM -p ipv6-icmp --icmpv6-type 128 -m conntrack --ctstate NEW -j ACCEPT
+	fi
+fi
+
+
+
+if [ "$selectedProtocols" != "1" ]
+then
+	iptables -N SSH
+	iptables -A NEW -p tcp --dport $sshPort -j SSH
+	iptables -A SSH -j ACCEPT
+fi
+
+if [ "$selectedProtocols" != "2" ]
+then
+	ip6tables -N SSH
+	ip6tables -A NEW -p tcp --dport $sshPort -j SSH
+	ip6tables -A SSH -j ACCEPT
+fi
 
 
 
@@ -295,16 +346,22 @@ ip6tables -A SSH -j ACCEPT
 echo "Setting default policies..."
 
 
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
+if [ "$selectedProtocols" != "1" ]
+then
+	iptables -P INPUT DROP
+	iptables -P FORWARD DROP
+	iptables -P OUTPUT ACCEPT
+fi
 
 
-ip6tables -P INPUT DROP
-ip6tables -P FORWARD DROP
-ip6tables -P OUTPUT ACCEPT
+if [ "$selectedProtocols" != "2" ]
+then
+	ip6tables -P INPUT DROP
+	ip6tables -P FORWARD DROP
+	ip6tables -P OUTPUT ACCEPT
+fi
 
 
 
-echo "iptables setup completed."
+echo "setup complete."
 
